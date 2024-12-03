6f9cb2859f2fda1c854651c41a24550fee6da92f
Jim Haaland Damgaard <jimdenseje@gmail.com>
2024-12-03 15:06:04 +0100

    made a small main program

--- Linked liste/Program.cs
+++ Linked liste/Program.cs
@@ -9,7 +9,28 @@ public class Program
         linkedList.AddElement(7);
         linkedList.AddElement(2);
         linkedList.AddElement(4);
+        Console.WriteLine(linkedList.Print());
+
         linkedList.Reverse();
+        Console.WriteLine(linkedList.Print());
+
+        linkedList.Sort(Order.DESC);
+        Console.WriteLine(linkedList.Print());
+
+        Console.WriteLine();
+        string stringlist = linkedList.Print();
+        string buffer = "";
+        int key = 0;
+        for (int x = 0; x < stringlist.Length; x++) {
+            if (stringlist[x] == ',') {
+                Console.WriteLine(key.ToString() + ": " + buffer);
+                buffer = "";
+                key++;
+            } else {
+                buffer += stringlist[x];
+            }
+        }
+        Console.WriteLine(key.ToString() + ": " + buffer);
     }
 
 }
